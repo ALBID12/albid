58585040803bbea591435c0eb692472308497cde
ALBID12 <105800163+ALBID12@users.noreply.github.com>
2022-05-18 07:27:00 -0700

    Add files via upload

---
+++ array.c
@@ -0,0 +1,27 @@
+//array declare
+//array intialize
+#include<stdio.h>
+int main()
+{
+    int sum=0,i,num[5];
+    printf("enter numbers");
+    for(i=0;i<5;i++){
+
+    scanf("%d",&num[i]);
+
+    }
+    for(i=0;i<5;i++)
+        {
+
+            sum = sum + num[i];
+        }
+    printf("%d\n",sum);
+    printf("%.2f",(float)sum/5);
+    /*num[0] = 10;
+    num[1] = 10;
+    num[2] = 10;
+    num[3] = 10;
+    num[4] = 10;*/
+    //sum = num[0]+num[1];
+    //printf("%d\n",sum);
+}
